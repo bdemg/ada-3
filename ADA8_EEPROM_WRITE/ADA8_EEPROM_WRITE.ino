838af8f5f09f3c4fbc2e951ef85c2a8f2dc56553
Jorge Cano <jacano.sosa@gmail.com>
2018-03-21 15:23:48 -0600

    Creación del programa para escribir notas a la EEPROM

--- ADA8_EEPROM_WRITE/ADA8_EEPROM_WRITE.ino
+++ ADA8_EEPROM_WRITE/ADA8_EEPROM_WRITE.ino
@@ -1,7 +1,24 @@
+#include <EEPROM.h>
 #include "tones.h"
 
 void setup() {
-    //Escribit a EEPROM
+    //Escribir tonos primer nivel
+    EEPROM.put(0, NOTE_D1);
+    EEPROM.put(1, NOTE_A1);
+    EEPROM.put(2, NOTE_D2);
+    
+    //Escribir tonos segundo nivel
+    EEPROM.put(3, NOTE_G1);
+    EEPROM.put(4, NOTE_D2);
+    EEPROM.put(5, NOTE_AS1);
+    EEPROM.put(6, NOTE_F2);
+    
+    //Escribir tonos tercer nivel
+    EEPROM.put(7, NOTE_C1);
+    EEPROM.put(8, NOTE_A1);
+    EEPROM.put(9, NOTE_D2);
+    EEPROM.put(10, NOTE_G2);
+    EEPROM.put(11, NOTE_E2);
 }
 
 
